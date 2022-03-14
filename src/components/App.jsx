b2e5ca9b229426affe0d15a5af99a03a7d09d891
Rico75 <rpeinado75@gmail.com>
2022-03-13 20:24:56 -0700

    working camera selection

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { Component, useState, useRef }	from 'react';
+import Dropdown							from 'react-bootstrap/Dropdown';
+import playGame 						from "../phaser/scene";
+import Player							from "../components/Player.jsx";
+import Cards							from "../components/Cards.jsx";
 
-export default class App extends React.Component {
+
+class App extends Component {
+
+	constructor(props)
+	{
+		super(props);
+		// Set initial state
+		this.state = {
+			numOfPlayers:			0,
+			dealerScore:			0,
+			dealerCards:			[],
+			showNumPlayerButton:	false
+		};
+
+		this.cards		= new Cards();
+		this.playGame 	= new playGame();
+		this.props = props;
+		sessionStorage.removeItem("howManyPlayer");
+
+
+		// Binding this keyword
+		this.updatePlayerNum = this.updatePlayerNum.bind(this);
+	}
+
+	// handle onChange event of the dropdown
+	// initialize the game
+	updatePlayerNum = val => {
+		this.setState({ numOfPlayers : val });
+		this.setState({ showNumPlayerButton : true });
+		sessionStorage.setItem('howManyPlayer', val);
+	}
+
 	render() {
 		return (
 			<div style={{ textAlign: "center" }}>
-				<h1>Hello World</h1>
+				<h1>Black Jack</h1>
+				<div>
+					<Dropdown onSelect={this.updatePlayerNum}>
+						<Dropdown.Toggle variant="primary" id="dropdown-basic" disabled={this.state.showNumPlayerButton}>
+							Select Number of Players: {this.state.numOfPlayers}
+						</Dropdown.Toggle>
+
+						<Dropdown.Menu>
+							<Dropdown.Item eventKey="1">1</Dropdown.Item>
+							<Dropdown.Item eventKey="2">2</Dropdown.Item>
+						</Dropdown.Menu>
+					</Dropdown>
+				</div>
 			</div>
 		);
 	}
 }
+
+export default App;
